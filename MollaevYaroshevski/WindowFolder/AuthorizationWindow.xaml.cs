af34d91aca912696d9720d7aa21a0dadae65653b
Дмитрий Талалаев <k.kommo@yandex.ru>
2023-01-16 20:08:48 +0300

    Добавьте файлы проекта.

---
+++ MollaevYaroshevski/WindowFolder/AuthorizationWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using MollaevYaroshevski.ClassFolder;
+using MollaevYaroshevski.DataFolder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MollaevYaroshevski.WindowFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void LoginBD_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MBClass.InfoMB("kommo");
+            if (string.IsNullOrWhiteSpace(PasswordPSB.Password))
+            {
+                MBClass.ErrorMB("Password");
+                PasswordPSB.Focus();
+            }
+            else
+            {
+                try
+                {
+                    var user = DBEntities.GetContext().User.FirstOrDefault(u => u.Login == LOginText.Text);
+                    if (user == null)
+                    {
+                        MBClass.ErrorMB("Incorect Login");
+                        return;
+                    }
+                    if (user.Password != PasswordPSB.Password)
+                    {
+                        MBClass.ErrorMB("Incorect PAssword");
+                        return;
+                    }
+                    else
+                    {
+                        switch (user.IdRole)
+                        {
+                            case 1:
+                                new AdminWindow().Show();
+                                break;
+                            case 2:
+                                MBClass.InfoMB("Rab");
+                                break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void RegBD_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            new RegistrationWindow().Show();
+            Close();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
---
+++ MollaevYaroshevski/WindowFolder/RegistrationWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using MollaevYaroshevski.ClassFolder;
+using MollaevYaroshevski.DataFolder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MollaevYaroshevski.WindowFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(LoginTB.Text))
+            {
+                MBClass.ErrorMB("Login");
+                LoginTB.Focus();
+            }
+            else if(DBEntities.GetContext().User.FirstOrDefault(u=>u.Login==LoginTB.Text) != null)
+            {
+                MBClass.ErrorMB("Существует");
+                LoginTB.Focus();
+            }
+            else if(string.IsNullOrWhiteSpace(PasswordPSB.Password))
+            {
+                MBClass.ErrorMB("НЕту");
+                
+            }
+            else if (string.IsNullOrWhiteSpace(RepeatPasswordPSB.Password))
+            {
+                MBClass.ErrorMB("Нету");
+            }
+            else
+            {
+                try
+                {
+                    DBEntities.GetContext().User.Add(new User()
+                    {
+                        Login=LoginTB.Text,
+                        Password=PasswordPSB.Password,
+                        IdRole=2
+                    });
+
+                    DBEntities.GetContext().SaveChanges();
+                    MBClass.InfoMB("Успешно");
+                }
+                catch ( Exception ex)
+                {
+                    
+                    MBClass.ErrorMB(ex);
+                }
+            }
+        }
+    }
+}
